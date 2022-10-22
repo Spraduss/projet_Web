a832f7fb27aa34423629ac2b6061a25a6dfd4661
Louis <louis.ruellou@gmail.com>
2022-10-22 13:43:36 +0200

    Update app.module.ts

--- fr-administrator/src/app.module.ts
+++ fr-administrator/src/app.module.ts
@@ -4,11 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AssociationsService } from './associations/associations.service';
 import { AssociationsController } from './associations/associations.controller';
-import { ApplicationsModule } from './applications/applications.module';
 import { AssociationsModule } from './associations/associations.module';
 
 @Module({
-  imports: [UsersModule, ApplicationsModule, AssociationsModule],
+  imports: [UsersModule, AssociationsModule],
   controllers: [AppController, AssociationsController],
   providers: [AppService, AssociationsService],
 })
